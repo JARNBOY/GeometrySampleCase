d1aa6d13305dccac2e853f438b5b14dad2760307
Papon S <papon.su@nocnonc.com>
2024-05-22 19:28:08 +0700

    Initial Commit

---
+++ GeometrySampleCase/GeometrySampleCaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeometrySampleCaseApp.swift
+//  GeometrySampleCase
+//
+//  Created by Papon Supamongkonchai on 22/5/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct GeometrySampleCaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GeometrySampleCaseUITests/GeometrySampleCaseUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  GeometrySampleCaseUITestsLaunchTests.swift
+//  GeometrySampleCaseUITests
+//
+//  Created by Papon Supamongkonchai on 22/5/2567 BE.
+//
+
+import XCTest
+
+final class GeometrySampleCaseUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
